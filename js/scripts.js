be920173e395eb63e06eea2a9c0c16722815f98b
Imandra McKenzie and Rakhat Tutueva <imandra@>
2019-02-11 20:37:11 -0800

    add survery form to collect contacts with CSS and JS.

--- js/scripts.js
+++ js/scripts.js
@@ -14,4 +14,33 @@ $(document).ready(function() {
 
     event.preventDefault();
   });
+
+
+
+$("#address-form form").submit(function(event){
+  var nameInput = $("input#name").val().charAt(0).toUpperCase() + $("input#name").val().slice(1);
+  var phoneInput = $("input#phone").val();
+  var emailInput = $("input#e-mail").val();
+  var streetAddressInput= $("input#street-address").val();
+  var cityInput = $("input#city").val().charAt(0).toUpperCase() + $("input#city").val().slice(1);
+  var stateInput = $("input#state").val().toUpperCase();
+  var zipCodeInput = $("input#zip-code").val();
+
+  $(".name").text(nameInput);
+  $(".phone").text(phoneInput);
+  $(".e-mail").text(emailInput);
+  $(".street-address").text(streetAddressInput);
+  $(".city").text(cityInput);
+  $(".state").text(stateInput);
+  $(".zip-code").text(zipCodeInput);
+
+  $("#contact-list").show();
+
+  event.preventDefault();
+  });
+
+
+  $("#name-click").click(function(){
+    $("li").show();
+ });
 });
